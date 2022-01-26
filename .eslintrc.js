7f720040ac8776e3cd80804d8835f9a4868f661b
Sergey Mangilev <ssmangilev@gmail.com>
2022-01-26 17:30:15 +0100

    feat: eslint add

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+    },
+    extends: [
+      "airbnb-base",
+      "airbnb-typescript/base",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended",
+      "plugin:import/errors",
+      "plugin:import/warnings",
+      "plugin:import/typescript",
+      "prettier",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: "module",
+      project: "./tsconfig.json",
+      tsConfigRootDir: "./",
+    },
+    plugins: ["@typescript-eslint", "prettier", "import"],
+    rules: {
+      "no-debugger": "off",
+      "no-console": 0,
+    },
+  };
+  
